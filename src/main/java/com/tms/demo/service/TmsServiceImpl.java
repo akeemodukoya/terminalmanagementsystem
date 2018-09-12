2dca8d9c1301e75a108ae552ad633d5ce0591783
gondiski <enochotieno@rocketmail.com>
2018-09-12 11:33:27 +0300

    Service implementation connection with repository

--- src/main/java/com/tms/demo/service/TmsServiceImpl.java
+++ src/main/java/com/tms/demo/service/TmsServiceImpl.java
@@ -6,11 +6,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.tms.demo.model.Application;
+import com.tms.demo.repository.ApplicationRepository;
+import com.tms.demo.service.TmsService;
 
 @Service
-public class TmsServiceImpl {
+public class TmsServiceImpl implements TmsService{
+	
+	private final
+	ApplicationRepository applicationRepository;
+	
 	public List<Application> findAllApplications(@PathVariable int id) {
 		 
 		return applicationRepository.findAll();
+	}
+
+	@Override
+	public List<com.tms.demo.model.Application> findAllApplications() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
