5fe96bbc220a01d14a33a17f5285871c61fb6f97
gondiski <enochotieno@rocketmail.com>
2018-09-13 10:09:44 +0300

    Added the user model

---
+++ src/main/java/com/tms/demo/model/User.java
@@ -0,0 +1,52 @@
+package com.tms.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String email;
+	private String first_name;
+	private String last_name;
+	
+	public User() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+}
