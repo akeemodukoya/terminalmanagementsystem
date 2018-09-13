8d4df8fc49d9d8a2dace27c337993034bcdb6f86
gondiski <enochotieno@rocketmail.com>
2018-09-13 12:10:25 +0300

    added controllers and fixed the link paths in the interface

---
+++ src/main/java/com/tms/demo/controller/UserTypeController.java
@@ -0,0 +1,15 @@
+package com.tms.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usertypes")
+public class UserTypeController {
+	@RequestMapping("/usertypes")
+	public String userTypes(Model model) {
+		model.addAttribute(".");
+		return "other-user-listing";
+	}
+}
