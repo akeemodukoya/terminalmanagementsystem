8cec875a108d6a999839bf00dcd6fd589fc35d51
gondiski <enochotieno@rocketmail.com>
2018-09-13 10:15:39 +0300

    Added project model

---
+++ src/main/java/com/tms/demo/model/Project.java
@@ -0,0 +1,35 @@
+package com.tms.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	
+	public Project() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
