ceb56a03789db2aa5eaf398745497684c3757acb
gondiski <enochotieno@rocketmail.com>
2018-09-17 14:52:38 +0300

    Implemented tms service in application controller

--- src/main/java/com/tms/demo/controller/ApplicationController.java
+++ src/main/java/com/tms/demo/controller/ApplicationController.java
@@ -1,32 +1,37 @@
 package com.tms.demo.controller;
 
-import java.net.URI;
+//import java.net.URI;
 //import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContextException;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.context.ApplicationContextException;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.tms.demo.model.Application;
-import com.tms.demo.repository.ApplicationRepository;
+//import com.tms.demo.model.Application;
+//import com.tms.demo.repository.ApplicationRepository;
+import com.tms.demo.service.TmsService;
 
 @Controller
 @RequestMapping("/")
 public class ApplicationController {
+	private final TmsService tmsService;
+	
 	@Autowired
-	private ApplicationRepository applicationRepository;
+	private ApplicationController(TmsService tmsService) {
+		this.tmsService = tmsService;
+	}
 	
 	
 	@RequestMapping("/applications")
 	public String application(Model model) {
-		model.addAttribute("", "");
+		model.addAttribute("applications", tmsService.findAllApplications());
 		return "task";
 	}
 //	public List<Application> retrieveAllApplications(@PathVariable int id) {
@@ -34,27 +39,27 @@ public class ApplicationController {
 //		return applicationRepository.findAll();
 //	}
 	
-	@RequestMapping("/applications/{id}")
-	public Application retrieveApplication(@PathVariable long id) {
-		Optional <Application> application = applicationRepository.findById(id);
-		
-		if (!application.isPresent())
-			throw new ApplicationContextException("id-" + id);
-		
-		return application.get();
-	}
-	
-	@RequestMapping("/application/{id}")
-	public void deleteApplication(@PathVariable long id) {
-		applicationRepository.deleteById(id);
-	}
-	
-	@RequestMapping("/application")
-	public ResponseEntity<Object> createApplication(@RequestBody Application application) {
-		Application savedApplication = applicationRepository.save(application);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
-				.buildAndExpand(savedApplication.getId()).toUri();
-		return ResponseEntity.created(location).build();
-	}
+//	@RequestMapping("/applications/{id}")
+//	public Application retrieveApplication(@PathVariable long id) {
+//		Optional <Application> application = applicationRepository.findById(id);
+//		
+//		if (!application.isPresent())
+//			throw new ApplicationContextException("id-" + id);
+//		
+//		return application.get();
+//	}
+//	
+//	@RequestMapping("/application/{id}")
+//	public void deleteApplication(@PathVariable long id) {
+//		applicationRepository.deleteById(id);
+//	}
+//	
+//	@RequestMapping("/application")
+//	public ResponseEntity<Object> createApplication(@RequestBody Application application) {
+//		Application savedApplication = applicationRepository.save(application);
+//		
+//		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+//				.buildAndExpand(savedApplication.getId()).toUri();
+//		return ResponseEntity.created(location).build();
+//	}
 }
