5f8b7454c62df53e6d45efbf2c1042374bdceda7
gondiski <enochotieno@rocketmail.com>
2018-09-17 11:07:30 +0300

    Added the service methods to find all

--- src/main/java/com/tms/demo/service/TmsService.java
+++ src/main/java/com/tms/demo/service/TmsService.java
@@ -3,9 +3,21 @@ package com.tms.demo.service;
 import java.util.List;
 
 import com.tms.demo.model.Application;
+import com.tms.demo.model.Project;
+import com.tms.demo.model.Terminal;
+import com.tms.demo.model.TerminalGroup;
+import com.tms.demo.model.User;
 
 //@Component
 public interface TmsService {
 	
-	List<Application> findAllApplications();
+	public List<Application> findAllApplications();
+	
+	public List<Project> findAllProjects();
+	
+	public List<Terminal> findAllTerminals();
+	
+	public List<TerminalGroup> findAllTerminalGroups();
+	
+	public List<User> findAllUsers();
 }
