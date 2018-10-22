4c04e41c753501d99b904d36120a6c66dc34b726
gondiski <enochotieno@rocketmail.com>
2018-10-22 14:54:53 +0300

    Added controller methods for login pages in user controller

--- src/main/java/com/tms/demo/controller/UserController.java
+++ src/main/java/com/tms/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tms.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -15,5 +16,17 @@ public class UserController {
 	@RequestMapping("/users/{id}")
 	public void delete() {
 		
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String loginForm() {
+		
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String loginSuccess() {
+		
+		return "index";
 	}
 }
