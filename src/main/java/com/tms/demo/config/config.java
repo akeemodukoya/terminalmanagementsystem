258d7d7289f8792b17b42a78ca69fe8b13177756
gondiski <enochotieno@rocketmail.com>
2018-10-25 12:08:14 +0300

    Added spring security dependency to pom file

--- src/main/java/com/tms/demo/config/config.java
+++ src/main/java/com/tms/demo/config/config.java
@@ -1,5 +1,17 @@
 package com.tms.demo.config;
 
-public class config {
 
+@EnableWebSecurity
+public class config extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest.authenticate()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll();
+	}
 }
