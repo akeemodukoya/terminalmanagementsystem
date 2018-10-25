a591d5f7c6286eac7a103ee5d22fa9824649a2ed
gondiski <enochotieno@rocketmail.com>
2018-10-25 12:29:51 +0300

    Fixed anyRequest error

--- src/main/java/com/tms/demo/config/config.java
+++ src/main/java/com/tms/demo/config/config.java
@@ -1,6 +1,9 @@
 package com.tms.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -10,7 +13,7 @@ public class Config extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest.authenticate()
+				.anyRequest().authenticated()
 				.and()
 			.formLogin()
 				.loginPage("/login")
