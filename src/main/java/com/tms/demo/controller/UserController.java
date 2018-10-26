68c4e6a710305c7f524a01f9c917f53ea568fde2
gondiski <enochotieno@rocketmail.com>
2018-10-26 13:56:09 +0300

    Added mysql branch

--- src/main/java/com/tms/demo/controller/UserController.java
+++ src/main/java/com/tms/demo/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.tms.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.tms.demo.service.TmsService;
+
 @Controller
 public class UserController {
+	
+	@Autowired
+	private TmsService tmsService;
+	
 	@RequestMapping("/users")
 	public String user(Model model) {
 		model.addAttribute("","");
@@ -20,7 +27,6 @@ public class UserController {
 	
 	@RequestMapping(value="/login", method = RequestMethod.POST)
 	public String login() {
-		
 		return "other-login";
 	}
 	
