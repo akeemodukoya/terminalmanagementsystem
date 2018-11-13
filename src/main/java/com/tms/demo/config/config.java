e142d0361c209ce3de2070aec40b9ed9f125b85b
gondiski <enochotieno@rocketmail.com>
2018-11-13 11:20:17 +0300

    Created login user method in controller

--- src/main/java/com/tms/demo/config/config.java
+++ src/main/java/com/tms/demo/config/config.java
@@ -3,6 +3,7 @@ package com.tms.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -38,6 +39,11 @@ public class Config extends WebSecurityConfigurerAdapter{
 				.and()
 			.logout()
 				.permitAll();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.TmsService(tmsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 }
--- src/main/java/com/tms/demo/controller/UserController.java
+++ src/main/java/com/tms/demo/controller/UserController.java
@@ -9,30 +9,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserController {
 	
-	
+//	listing all users that are present in the system
 	@RequestMapping("/users")
 	public String user(Model model) {
 		model.addAttribute("user","User");
 		return "table";
 	}
 	
+//	deleting a user
 	@RequestMapping(value="/user/{id}", method= RequestMethod.DELETE)
 	public void delete() {
 		
 	}
 	
+//	login method for a user
 	@RequestMapping(value="/login", method = RequestMethod.GET)
-	public String login(Model model) {
-		
+	public String login(Model model, String error, String logout) {
+		if (error != null)
+			model.addAttribute("error", "Email and Password do not match!")
+		if (logout != null)
+			model.addAttribute("logout", "User successfully logged out.")
 		return "other-login";
 	}
 	
+//	login error page
 	@RequestMapping("/login-error")
 	public String loginError(Model model){
 		model.addAttribute("loginError", true);
 		return "login-error";
 	}
 	
+//	logout page
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
 	public String logout(Model model) {
 		model.addAttribute("title", "Logout");
